5754c008c74887c1041eb480812bc2a1882e51d2
federicopieklo <federicopieklo@gmail.com>
2019-12-18 11:28:15 -0300

    9 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-//import { HeroServiceService } from './hero.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   //providers: [HeroServiceService],
   bootstrap: [AppComponent],
